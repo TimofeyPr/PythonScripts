dab34e95bd217c797b2577ed0ab892854b2cd253
TimofeyPr <42960508+TimofeyPr@users.noreply.github.com>
2018-09-05 11:49:33 +0300

    Add output to file

--- dirTree.py
+++ dirTree.py
@@ -1,18 +1,36 @@
 # Построение дерева каталогов и файлов. Корневой каталог запрашивается.
-import os 
-def CreateDirTree(rootDir):
+import os
+
+dirName  = ""
+
+def Output(str1, filename):
+    if len(filename) > 0:
+        f.write(str1 + '\n')
+    else:
+        print (str1)
+    
+def CreateDirTree(rootDir, filename):
     for lists in os.listdir(rootDir):
         path = os.path.join(rootDir, lists)
         if os.path.isdir(path):
-            print(path)
-            CreateDirTree(path)
+            Output(' '*(len(rootDir)-len(dirName)) + path[len(rootDir):] +'\\', filename)
+            CreateDirTree(path, filename)
     for lists in os.listdir(rootDir):
         path = os.path.join(rootDir, lists)
         if not(os.path.isdir(path)):
             lenDir = len(rootDir)
-            print (' '*lenDir + path[lenDir:])
+            Output (' '*(len(rootDir)-len(dirName)+1) + path[lenDir+1:], filename)
 
 dirName = input('Directory: ')
-CreateDirTree(dirName)
+filename = input ("Имя файла для вывода результатов (пустое имя - вывод в консоль): ")
+if len(filename) > 0:
+    f = open(filename, 'w', encoding='utf-8')
+    f.write('Содержимое папки ' + dirName + '\n')
 
+print ('Содержимое папки ' + dirName)   
+CreateDirTree(dirName, filename)
       
+if len(filename) > 0:
+    f.close()
+
+input()
