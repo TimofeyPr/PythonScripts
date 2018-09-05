da82ace9f7062a65d6c0add9575f22b1d623707b
TimofeyPr <42960508+TimofeyPr@users.noreply.github.com>
2018-09-05 14:29:06 +0300

    Add exception workaround

--- dirTree.py
+++ dirTree.py
@@ -11,23 +11,30 @@ def Output(str1, filename):
     
 def CreateDirTree(rootDir, filename):
     for lists in os.listdir(rootDir):
-        path = os.path.join(rootDir, lists)
-        if os.path.isdir(path):
-            Output(' '*(len(rootDir)-len(dirName)) + path[len(rootDir):] +'\\', filename)
-            CreateDirTree(path, filename)
+        try:      
+            path = os.path.join(rootDir, lists)
+            if os.path.isdir(path):
+                Output(' '*(len(rootDir)-len(dirName)) + path[len(rootDir):] +'\\', filename)
+                CreateDirTree(path, filename)
+        except:
+            Output(' '*(len(rootDir)-len(dirName)) + path[len(rootDir):] +'\\ - нет доступа', filename)
     for lists in os.listdir(rootDir):
         path = os.path.join(rootDir, lists)
         if not(os.path.isdir(path)):
             lenDir = len(rootDir)
-            Output (' '*(len(rootDir)-len(dirName)+1) + path[lenDir+1:], filename)
+            if len(rootDir)-len(dirName) > 0:
+                Output (' '*(len(rootDir)-len(dirName)+1) + path[lenDir+1:], filename)
+            else:
+                Output (path[lenDir:], filename)
 
-dirName = input('Directory: ')
+dirName = input('Введите путь к папке: ')
 filename = input ("Имя файла для вывода результатов (пустое имя - вывод в консоль): ")
 if len(filename) > 0:
     f = open(filename, 'w', encoding='utf-8')
     f.write('Содержимое папки ' + dirName + '\n')
+else:
+    print ('Содержимое папки ' + dirName)   
 
-print ('Содержимое папки ' + dirName)   
 CreateDirTree(dirName, filename)
       
 if len(filename) > 0:
