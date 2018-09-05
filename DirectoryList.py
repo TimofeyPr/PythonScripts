ce44f106b3715ab35d85d5d0f4c9dd4145e007ce
TimofeyPr <42960508+TimofeyPr@users.noreply.github.com>
2018-09-05 11:48:49 +0300

    Add directory and file symbols

--- DirectoryList.py
+++ DirectoryList.py
@@ -3,6 +3,8 @@ list1 = os.listdir()
 filename = input('Имя файла для вывода результатов: ')
 f = open(filename, 'w', encoding='utf-8')
 for n in range (0, len(list1)):
-        f.write (list1[n]+'\n')
+        if os.path.isdir(list1[n]):
+                f.write ('+- ' + list1[n] + '\n')
+        else:
+                f.write ('-- ' + list1[n] + '\n')
 f.close()
-input()
